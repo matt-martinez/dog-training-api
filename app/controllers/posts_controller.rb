7f203cdd77919985899b99f03c3f1288b636029e
mattmartinez <mattmartinez816@gmail.com>
2017-03-13 22:11:58 -0700

    Post create written and tested

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,4 +8,19 @@ class PostsController < ApplicationController
     render json: { post: Post.find(params[:id]) }
   end
 
+  def createPost
+    post = Post.new(post_params)
+
+    if post.save
+      render json: { status: 201, post: post }
+    else
+      render json: { status: 422 }
+    end
+  end
+
+  private
+
+    def post_params
+      params.require(:post).permit(:title, :body, :user_id)
+    end
 end
