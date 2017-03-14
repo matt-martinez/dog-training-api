2ed73afb7a0e17a4fc93552f04ddd3d876c3fa1f
mattmartinez <mattmartinez816@gmail.com>
2017-03-13 22:16:52 -0700

    Post update written and tested

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -18,6 +18,16 @@ class PostsController < ApplicationController
     end
   end
 
+  def update
+    post = Post.find(params[:id])
+
+    if post.update(post_params)
+      render json: { status: 201, post: post }
+    else
+      render json: { status: 422 }
+    end
+  end
+
   private
 
     def post_params
