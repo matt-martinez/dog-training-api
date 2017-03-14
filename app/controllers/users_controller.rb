0ac7ba71fc23abc65c1c9f03a543bfcd18721190
mattmartinez <mattmartinez816@gmail.com>
2017-03-13 20:52:35 -0700

    User create written and tested

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,19 @@
 class UsersController < ApplicationController
+  before_action :authorize, except: [:login, :create]
+
+  def create
+    user = User.new(user_params)
+
+    if user.save
+      render json: { status: 200, message: "ok" }
+    else
+      render json: { status: 422, user: user.errors }
+    end
+  end
+
+
+  def user_params
+    params.required(:user).permit(:name, :username, :email, :password)
+  end
+
 end
