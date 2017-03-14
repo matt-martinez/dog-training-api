d7151ce189b895280b0de0de54189a93c5c858e1
mattmartinez <mattmartinez816@gmail.com>
2017-03-13 21:21:04 -0700

    Finished auth, tested login and user show

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,30 @@
 class ApplicationController < ActionController::API
+  def authenticate
+    render json: {status: 401, message: "unauthorized"} unless decode_token(bearer_token)
+  end
+
+  def authorize
+    render json: {status: 401, message: "unauthorized"} unless current_user.id == params[:id].to_i
+  end
+
+  def bearer_token
+    pattern = /^Bearer /
+    header  = request.env["HTTP_AUTHORIZATION"] # <= env
+    header.gsub(pattern, '') if header && header.match(pattern)
+  end
+
+  def current_user
+    return if !bearer_token
+
+    decoded_jwt = decode_token(bearer_token)
+
+    @current_user ||= User.find(decoded_jwt["user"]["id"])
+  end
+
+  def decode_token(token)
+    @token ||= JWT.decode(token, nil, false)[0]
+
+  rescue
+    render json: {status: 401, message: 'invalid or expired token'}
+  end
 end
