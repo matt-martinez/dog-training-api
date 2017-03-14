6d6490918dc1f74be45e9626db8dd1aa45e3fbbd
mattmartinez <mattmartinez816@gmail.com>
2017-03-13 20:25:57 -0700

    Added custom routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
-  resources :users
-  resources :posts
+  resources :comments, only: [:index, :show, :update, :destroy]
+  resources :posts, only: [:index, :show, :update, :destroy]
+  resources :users, only: [:create, :show] do
+    collection do
+      post '/login', to: 'users#login'
+    end
+  end
+  # get 'posts/:id/comments', 'comments#commentsByPost'
+  # get 'users/:id/comments', 'comments#commentsByUser'
+  # # post 'posts/:id/comments', to 'comments#createComment'
+  # # post 'users/:id/posts', to 'posts#createPost'
 end
