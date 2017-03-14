1c3e0f1809153d9ab6347193276e934f947ef96d
mattmartinez <mattmartinez816@gmail.com>
2017-03-13 22:29:16 -0700

    Comment create written and tested

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -8,4 +8,20 @@ class CommentsController < ApplicationController
     render json: { comment: Comment.find(params[:id]) }
   end
 
+  def createComment
+    comment = Comment.new(comment_params)
+
+    if comment.save
+      render json: { status: 201, comment: comment }
+    else
+      render json: { status: 422 }
+    end
+  end
+
+
+  private
+
+    def comment_params
+      params.require(:comment).permit(:body, :user_id, :post_id)
+    end
 end
