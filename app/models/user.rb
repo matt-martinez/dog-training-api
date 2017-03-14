8dedc20310167f36b75cd39ff4c79921717f46a1
mattmartinez <mattmartinez816@gmail.com>
2017-03-14 09:38:04 -0700

    Updated user model to have many behaviors, added validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
+  validates :email, presence: true, uniqueness: true
   has_secure_password
 
   has_many :posts, dependent: :destroy
   has_many :comments, through: :posts
+  has_many :behaviors
 end
