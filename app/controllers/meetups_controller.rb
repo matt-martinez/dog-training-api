fdd5d737eb8b8c024783ae8029e74ee6ef9f1111
mattmartinez <mattmartinez816@gmail.com>
2017-03-17 12:24:50 -0700

    Refactored meetup controller

--- app/controllers/meetups_controller.rb
+++ app/controllers/meetups_controller.rb
@@ -1,14 +1,7 @@
 class MeetupsController < ApplicationController
 
   def search
-    zip = params[:zip]
-    radius = params[:radius]
-
-    puts zip
-
-    api_key = ENV['API_KEY']
-
-    meetup_groups = HTTParty.get("https://api.meetup.com/find/groups?zip=#{zip}&radius=#{radius}&category=26&order=members&key=#{api_key}")
+    meetup_groups = Meetup.search(params[:zip], params[:radius])
 
     render json: { groups: meetup_groups }
   end
--- app/models/meetup.rb
+++ app/models/meetup.rb
@@ -1,2 +1,9 @@
 class Meetup < ApplicationRecord
+
+
+  def self.search(zip, radius)
+    api_key = ENV['API_KEY']
+
+    HTTParty.get("https://api.meetup.com/find/groups?zip=#{zip}&radius=#{radius}&category=26&order=members&key=#{api_key}")
+  end
 end
